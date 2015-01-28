c0e402da9efdab08361bc429d56409b559b193de
wires <jelle@defekt.nl>
2015-01-28 20:56:54 +0100

    testing ISvg reloading

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var React = require("react");
+var ISvg = require("react-inlinesvg");
+
+var App = React.createClass({
+    getInitialState: function () {
+        return { files: ["test-a.svg", "test-b.svg"] };
+    },
+    render: function () {
+        var fn = this.state.files[Math.floor(Math.random()*2)];
+        console.log(fn)
+        return (<ISvg src={ fn }/>);
+    }
+});
 
 window.addEventListener('DOMContentLoaded', function(){
-    console.log("DOM => loaded");
-    React.render(<p>React was here</p>, document.getElementById('content'));
+    setInterval(function(){
+        React.render(
+            <App/>
+            , document.getElementById('content'));
+    }, 500);
 });
