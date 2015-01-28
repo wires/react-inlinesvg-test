03abb58b156fbd32645f1798428f2ef896d116de
wires <jelle@defekt.nl>
2015-01-28 21:21:46 +0100

    using react-router

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 var React = require("react");
 var ISvg = require("react-inlinesvg");
+var Router = require('react-router');
+
+
+var FILES = ["test-a.svg", "test-b.svg"];
 
 var App = React.createClass({
-    propTypes: {
-        index: React.PropTypes.number.isRequired
-    },
     render: function () {
-        var FILES = ["test-a.svg", "test-b.svg"];
-        var fn = FILES[this.props.index];
-        console.log(fn)
-        return (<ISvg src={ fn }/>);
+        var links = FILES.map(function(filename){
+            return (
+                <li>
+                    <Router.Link to="page" params={{filename:filename}}>
+                        {filename}
+                    </Router.Link>
+                </li>
+            )
+        })
+
+        return (
+            <div>
+            <ul>{links}</ul>
+            <Router.RouteHandler/>
+            </div>
+        )
     }
 });
 
+var SVGPage = React.createClass({
+    mixins: [Router.State],
+    render: function() {
+        console.log("rendering =>", this.getParams().filename);
+        return (<ISvg src={ this.getParams().filename }/>);
+    }
+})
+
+var NothingYet = React.createClass({
+    render: function () {
+        return <p>Select a page above</p>;
+    }
+});
+
+var routes = (
+    <Router.Route handler={App}>
+        <Router.DefaultRoute handler={NothingYet}/>
+        <Router.Route name="page" path="page/:filename" handler={SVGPage}/>
+    </Router.Route>
+)
+
 window.addEventListener('DOMContentLoaded', function(){
-    setInterval(function(){
-        var i = Math.floor(Math.random()*2); // 0 or 1
-        React.render(
-            <App index={i}/>
-            , document.getElementById('content'));
-    }, 500);
+    Router.run(routes, function (Handler) {
+        React.render(<Handler/>, document.getElementById('content'));
+    });
 });
